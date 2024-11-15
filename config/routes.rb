04e6becb4eadb53ebdf07f740cec71f10978b28b
Sarah Lepilleur <sarah.lepilleur@sciencespo.fr>
2024-11-15 16:39:22 +0100

    edit of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "lists#index"
-  resources :lists, except: :index do
+  resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
